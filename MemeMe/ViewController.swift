5d37ffec3dba53639a3a911791cd09c730c03bf4
Hassan Jamila <hassan.jamila@n2nconnect.com>
2020-01-18 16:49:04 +0800

    Launch the image view controller.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  , UIPageViewControllerDelegate , UINavigationControllerDelegate{
 
+    @IBOutlet weak var topLabel: UITextField!
+    @IBOutlet weak var bottomLabel: UITextField!
+    @IBOutlet weak var mainImageView: UIImageView!
+
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let isCameraAvailable = UIImagePickerController.isSourceTypeAvailable(.camera)
+        if !isCameraAvailable {
+            cameraButton.isEnabled = false
+        }
+        
+    }
+    
+    @IBAction func shareImage(_ sender: Any) {
+    }
+    
+    @IBAction func saveImage(_ sender: Any) {
+    }
+    
+    
+    @IBAction func getImageViaCamera(){
+        showImagePickerContoller(isCamera: true)
+    }
+    
+    @IBAction func getImageViaGallery(){
+        showImagePickerContoller(isCamera: false)
     }
 
+    
+    func showImagePickerContoller(isCamera:Bool){
+        let contoller = UIImagePickerController()
+        contoller.delegate  = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        if isCamera {
+            contoller.sourceType = .camera
+        }
+        present(contoller, animated: true, completion: nil)
+        
+    }
 
 }
 
